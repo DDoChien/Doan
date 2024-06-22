5a3ba6ffe9b0afbbb0201ca7a350a2f1e93deb26
chien <john.doe@example.com>
2024-06-22 16:53:07 +0700

    first commit1

---
+++ Doan_Coso/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using App_Web.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace App_Web.Areas.Admin.Controllers
+{
+    [Authorize(Policy = "RequireOwnerAndSaleRole")]
+    [Area("Admin")]
+    public class OrdersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public OrdersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Orders
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Orders
+                .Include(o => o.User)
+                .Include(o => o.OrderDetails).ThenInclude(p => p.Product)
+                .OrderByDescending(o => o.OrderDate); // Sắp xếp đơn hàng theo ngày đặt hàng giảm dần
+
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Admin/Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.User)
+                .Include(o => o.OrderDetails).ThenInclude(p => p.Product)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "UserID", "Email");
+            return View();
+        }
+
+        // POST: Admin/Orders/Create
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("OrderId,UserId,OrderDate,ShipDate,PaymentDate,PaymentId,Note,CreatedAt,ShipperId,Status,PaymentMethod,ShippingAddress,ContactPhone,TotalPrice")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserID", "Email", order.UserId);
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserID", "Email", order.UserId);
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Edit/5
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("OrderId,UserId,OrderDate,ShipDate,PaymentDate,PaymentId,Note,CreatedAt,ShipperId,Status,PaymentMethod,ShippingAddress,ContactPhone,TotalPrice")] Order order)
+        {
+            if (id != order.OrderId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.OrderId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserID", "Email", order.UserId);
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.User)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Confirm(int id)
+        {
+            var order = await _context.Orders.FirstOrDefaultAsync(o => o.OrderId == id);
+            if (order != null)
+            {
+                var orderDetails = await _context.Orderdetails.FirstOrDefaultAsync(o => o.OrderId == id);
+                var product = await _context.Products.FirstOrDefaultAsync(o => o.ProductId == orderDetails.ProductId);
+                order.Status = "Đã xác nhận";
+                _context.Orders.Update(order);
+                await _context.SaveChangesAsync();
+                return Redirect("/Admin/Orders");
+            }
+            return NotFound();
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.OrderId == id);
+        }
+    }
+}
